3271bd9f867e2f9d13dfab9022728c349fa50d97
gradymcd2 <gmcdowel@stevens.edu>
2021-05-17 23:15:35 -0400

    added inputs, did some other stuff

--- ls.py
+++ ls.py
@@ -10,23 +10,23 @@ def apply_rules(axiom):
 
 def draw(M):
 	for m in M:
-		if m == 'F':
+		if m == 'F' or m == 'G':
 			t.forward(distance)
 		if m == 'B':
 			t.backward(distance)
-		if m == 'L':
+		if m == 'L' or m == '+':
 			t.left(angle)
-		if m == 'R':
+		if m == 'R' or m == '-':
 			t.right(angle)
 		if m == '[': # push position and angle, turn left 45 degrees
 			stack.append([t.pos(), t.heading()])
-			t.left(angle)
+			#t.left(angle)
 		if m == ']': # pop position and angle, turn right 45 degrees
 			p = stack.pop()
 			t.pu()
 			t.setpos(p[0])
 			t.setheading(p[1])
-			t.right(angle)
+			#t.right(angle)
 			t.pd()
 		if m == '0' or m == '1': # draw a line segment ending in a leaf
 			t.forward(distance)
@@ -36,7 +36,8 @@ rulesstr = input("Rules(1:11, 0:1[0]0): ")
 if rulesstr == '':
 	rules = {'1':'11', '0':'1[0]0'}
 else:
-	rules = ast.literal_eval('{' + re.sub("([A-Za-z0-9\[\]]+):([A-Za-z0-9\[\]]+)", r'"\1":"\2"', rulesstr) + '}')
+	rules = ast.literal_eval('{' + re.sub("([A-Za-z0-9\[\]\-\+]+):([A-Za-z0-9\[\]\-\+]+)", r'"\1":"\2"', rulesstr) + '}')
+print(rules)
 iterations = input("Iterations (4): ")
 if iterations == '':
 	iterations = '4'
@@ -49,19 +50,26 @@ distance = input("Distance (20): ")
 if distance == '':
 	distance = '20'
 distance = int(distance)
+axiom = input("Axiom (0): ")
+if axiom == '':
+	axiom = '0'
+sab = input("Start at bottom instead of center (y): ")
+if sab == '':
+	sab = 'y'
 
 stack = []
 
 t = turtle.Turtle()
-M = apply_rules("0")
+M = apply_rules(axiom)
 print("Result string: ", M)
 
-t.pu()
-t.left(90)
-t.back(512)
-t.pd()
-t.speed(10)
+if(sab.lower() == 'y'):
+	t.pu()
+	t.left(90)
+	t.back(512)
+	t.pd()
 
+t.speed(10)
 draw(M)
 input("press enter to exit")
-t.done()
+turtle.done()
